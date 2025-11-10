d19070b131abacd214249b5fa5b562d47e905da6
kevenparcmiyeno <kevenparcmiyeno@users.noreply.github.com>
2025-11-10 13:07:22 +0000

    Add analytics.jsx, import-import.md, payment-static.rs

---
+++ frontend/web/src/configuration-simple-registry/payment-static.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/corpvote/frontend/web/src/configuration-simple-registry/payment-static.rs
+// Generated at: 2025-11-27T12:25:20.629Z
+
